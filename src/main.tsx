36f952c3a00301f3188573ee6446fb22914a8755
khuong.mai@nexpando <maikhuong2003@gmail.com>
2024-04-29 18:24:39 +0700

    fix for github page deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, createHashRouter, HashRouter } from 'react-router-dom';
+
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
 
