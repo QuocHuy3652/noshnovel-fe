ea65bd2b07b68bf147e424d2c9e400f60f32bcff
khuong.mai@nexpando <maikhuong2003@gmail.com>
2024-04-29 18:37:17 +0700

    fix routing for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,27 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter, createHashRouter, HashRouter, RouterProvider } from 'react-router-dom';
+import { Dashboard } from '~/pages';
 
 
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
+// const router = createHashRouter([
+//   {
+//     path: "/noshnovel-fe",
+//     element: <App />,
+//     children:[
+//       {
+//         path: "/",
+//         element: <Dashboard />
+//
+//       }
+//     ]
+//   }
+// ]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
 );
 
