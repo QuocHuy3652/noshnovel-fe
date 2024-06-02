bdee42ae3293f7988df3e93ed7b498bbae44a937
khuong.mai@nexpando <maikhuong2003@gmail.com>
2024-06-02 23:42:34 +0700

    fix home href

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
     <>
       <div className="navbar-wrapper flex min-h-[1.5rem] bg-app_primary p-2 sticky top-0 z-50">
         <nav className="navbar">
-          <a href="/" className="navbar-brand flex flex-row items-center">
+          <a href="/noshnovel-fe" className="navbar-brand flex flex-row items-center">
             <img className="w-[3rem] h-[3rem]" src={nosh_logo} alt="logo" />
             <p className="nosh-logo-text text-2xl">Nosh Novel</p>
           </a>
